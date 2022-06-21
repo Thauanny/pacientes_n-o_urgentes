6a4850cffc795f84845a9f691492ab4ea9354672
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-06-20 21:50:45 -0300

    ajuste em porta

--- spring mvc pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02ApplicationMVC.java
+++ spring mvc pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02ApplicationMVC.java
@@ -3,16 +3,16 @@ package com.traballhounid02.trabalho02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableEurekaClient	
+@EnableEurekaClient
 public class Trabalho02ApplicationMVC {
-
+	@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(Trabalho02ApplicationMVC.class, args);
 	}
 
 }
- 
