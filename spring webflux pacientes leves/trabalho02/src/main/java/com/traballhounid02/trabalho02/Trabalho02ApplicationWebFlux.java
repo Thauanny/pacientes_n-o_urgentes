ed87c4d24c0e1445767b91b1f9d4dc64c79e7a48
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-06-20 21:50:10 -0300

    ajuste em porta

--- spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02ApplicationWebFlux.java
+++ spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02ApplicationWebFlux.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableEurekaClient
 public class Trabalho02ApplicationWebFlux {
-
+	@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(Trabalho02ApplicationWebFlux.class, args);
 	}
