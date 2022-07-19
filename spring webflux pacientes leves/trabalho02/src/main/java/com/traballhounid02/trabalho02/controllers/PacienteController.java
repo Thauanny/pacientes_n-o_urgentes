d229f95e0793548ace5fc8e093909be7a12714c0
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-07-18 22:24:22 -0300

    kafka, config, cb

--- spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/controllers/PacienteController.java
+++ spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/controllers/PacienteController.java
@@ -1,38 +1,33 @@
 package com.traballhounid02.trabalho02.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
-import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.traballhounid02.trabalho02.models.Paciente;
 import com.traballhounid02.trabalho02.services.PacienteService;
 
+import reactor.core.publisher.Flux;
 
 
-@Controller
+
+@RestController
+@RequestMapping("/leves")
 public class PacienteController {
 
     @Autowired  
     private PacienteService pacienteService;
 
-    @GetMapping(path  = "/leves")
-    public String findNotEmergency(final Model model) {
+    @GetMapping(path  = "/")
+    public Flux<Paciente>  findNotEmergency() {
 
-        IReactiveDataDriverContextVariable reactiveDataDrivenMode =
-            new ReactiveDataDriverContextVariable( this.pacienteService.findNotEmergency());
-  
-        model.addAttribute("pacientes", reactiveDataDrivenMode);
-
-        return "pacientes/index";
+       return this.pacienteService.findNotEmergency();
     }
 
     @GetMapping(value = "/leves/{id}/delete")
-    public String delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id){
         this.pacienteService.deleteById(id);
-        return "redirect:http://localhost:9000/LEVES/leves";
+        
     }
-
-
 }
--- spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/services/PacienteService.java
+++ spring webflux pacientes leves/trabalho02/src/main/java/com/traballhounid02/trabalho02/services/PacienteService.java
@@ -1,10 +1,6 @@
 package com.traballhounid02.trabalho02.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.domain.ExampleMatcher.StringMatcher;
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.stereotype.Service;
 import com.traballhounid02.trabalho02.models.Paciente;
 import com.traballhounid02.trabalho02.repositories.PacienteRepository;
@@ -15,8 +11,6 @@ public class PacienteService {
     
     @Autowired
     private PacienteRepository pacienteRepository;
-
-
    
     public Flux<Paciente> findNotEmergency(){
         return this.pacienteRepository.findNotEmergency();
@@ -25,7 +19,6 @@ public class PacienteService {
     public void deleteById(Long id){
         this.pacienteRepository.deleteById(id).subscribe();
 
-    }
-    
+    } 
      
 }
